873599ec19d088dbc6cab24bd3f256d7a83b1dc7
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-29 13:15:00 +0300

    adding ul items

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -8,6 +8,30 @@ function Navbar() {
                 <Image src={LOGO} alt="logo" layout="fill"
                     objectFit="contain" objectPosition="left" />
             </div>
+
+            {/* navitems part  */}
+            <div className="hidden md:flex items-center space-x-1">
+                <a
+                    href="/"
+                    className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
+                >Home</a
+                >
+                <a
+                    href="/"
+                    className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+                >Services</a
+                >
+                <a
+                    href="/"
+                    className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+                >About</a
+                >
+                <a
+                    href="/"
+                    className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+                >Contact Us</a
+                >
+            </div>
         </header>
     )
 }
