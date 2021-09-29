5d4172e86a0319c56e907f6b62620f76f08814f5
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-29 13:50:41 +0300

    back tosta

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,9 +1,18 @@
-import React from "react";
+import Image from "next/image"
+import LOGO from "../../public/logo.png"
+import Link from 'next/link'
 
-export default function Navbar() {
+function Navbar() {
     return (
-        <>
-            thing aboout next move
-        </>
-    );
+        <header className="sticky top-0 z-50 gride grid-cols-3 bg-white shadow-md py-5 p-5 md:px-10 ">
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+                <Image src={LOGO} alt="logo" layout="fill"
+                    objectFit="contain" objectPosition="left" />
+            </div>
+            {/* navitems */}
+
+        </header>
+    )
 }
+
+export default Navbar
