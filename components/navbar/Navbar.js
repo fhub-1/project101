560e9b65db4508d6ef911a89cf297f82ba1ef9c4
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-29 13:43:32 +0300

    adding another code

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,41 +1,156 @@
-import Image from "next/image"
-import LOGO from "../../public/logo.png"
-import Link from 'next/link'
+import React from "react";
 
-function Navbar() {
+export default function Navbar(props) {
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
     return (
-        <header className="sticky top-0 z-50 gride grid-cols-3 bg-white shadow-md py-5 p-5 md:px-10 ">
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
-                <Image src={LOGO} alt="logo" layout="fill"
-                    objectFit="contain" objectPosition="left" />
-            </div>
-            {/* navitems */}
-            <div className="hidden md:flex items-center space-x-1">
-                <ul>
-                    <li className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold">
-                        <Link href="/">
-                            <a>Home</a>
-                        </Link>
-                    </li>
-                    <li className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold ">
-                        <Link href="/">
-                            <a>Home</a>
-                        </Link>
-                    </li>
-                    <li className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold ">
-                        <Link href="/">
-                            <a>Home</a>
-                        </Link>
-                    </li>
-                    <li className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold">
-                        <Link href="/">
-                            <a>Home</a>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </header>
-    )
-}
+        <>
+            <nav
+                className={
+                    (props.transparent
+                        ? "top-0 absolute z-50 w-full"
+                        : "relative shadow-lg bg-white shadow-lg") +
+                    " flex flex-wrap items-center justify-between px-2 py-3 "
+                }
+            >
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <a
+                            className={
+                                (props.transparent ? "text-white" : "text-gray-800") +
+                                " text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                            }
+                            href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
+                        >
+                            Tailwind Starter Kit
+                        </a>
+                        <button
+                            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <i
+                                className={
+                                    (props.transparent ? "text-white" : "text-gray-800") +
+                                    " fas fa-bars"
+                                }
+                            ></i>
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none" +
+                            (navbarOpen ? " block rounded shadow-lg" : " hidden")
+                        }
+                        id="example-navbar-warning"
+                    >
+                        <ul className="flex flex-col lg:flex-row list-none mr-auto">
+                            <li className="flex items-center">
+                                <a
+                                    className={
+                                        (props.transparent
+                                            ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                                            : "text-gray-800 hover:text-gray-600") +
+                                        " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                    }
+                                    href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/login"
+                                >
+                                    <i
+                                        className={
+                                            (props.transparent
+                                                ? "lg:text-gray-300 text-gray-500"
+                                                : "text-gray-500") +
+                                            " far fa-file-alt text-lg leading-lg mr-2"
+                                        }
+                                    />{" "}
+                                    Docs
+                                </a>
+                            </li>
+                        </ul>
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="flex items-center">
+                                <a
+                                    className={
+                                        (props.transparent
+                                            ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                                            : "text-gray-800 hover:text-gray-600") +
+                                        " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                    }
+                                    href="#pablo"
+                                >
+                                    <i
+                                        className={
+                                            (props.transparent
+                                                ? "lg:text-gray-300 text-gray-500"
+                                                : "text-gray-500") +
+                                            " fab fa-facebook text-lg leading-lg "
+                                        }
+                                    />
+                                    <span className="lg:hidden inline-block ml-2">Share</span>
+                                </a>
+                            </li>
 
-export default Navbar
+                            <li className="flex items-center">
+                                <a
+                                    className={
+                                        (props.transparent
+                                            ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                                            : "text-gray-800 hover:text-gray-600") +
+                                        " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                    }
+                                    href="#pablo"
+                                >
+                                    <i
+                                        className={
+                                            (props.transparent
+                                                ? "lg:text-gray-300 text-gray-500"
+                                                : "text-gray-500") +
+                                            " fab fa-twitter text-lg leading-lg "
+                                        }
+                                    />
+                                    <span className="lg:hidden inline-block ml-2">Tweet</span>
+                                </a>
+                            </li>
+
+                            <li className="flex items-center">
+                                <a
+                                    className={
+                                        (props.transparent
+                                            ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                                            : "text-gray-800 hover:text-gray-600") +
+                                        " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                    }
+                                    href="#pablo"
+                                >
+                                    <i
+                                        className={
+                                            (props.transparent
+                                                ? "lg:text-gray-300 text-gray-500"
+                                                : "text-gray-500") +
+                                            " fab fa-github text-lg leading-lg "
+                                        }
+                                    />
+                                    <span className="lg:hidden inline-block ml-2">Star</span>
+                                </a>
+                            </li>
+
+                            <li className="flex items-center">
+                                <button
+                                    className={
+                                        (props.transparent
+                                            ? "bg-white text-gray-800 active:bg-gray-100"
+                                            : "bg-pink-500 text-white active:bg-pink-600") +
+                                        " text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                                    }
+                                    type="button"
+                                    style={{ transition: "all .15s ease" }}
+                                >
+                                    <i className="fas fa-arrow-alt-circle-down"></i> Download
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
