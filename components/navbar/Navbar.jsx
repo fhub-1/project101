d899cc8ae4fd31dc4b8d4a992041b0797506bb9f
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-29 13:08:56 +0300

    adding header

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import LOGO from "../../public/logo.png"
 
 function Navbar() {
     return (
-        <header>
-            <div>
+        <header className="sticky top-0 z-50 gride grid-cols-3 bg-white shadow-md py-5 px-5">
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image src={LOGO} alt="logo" layout="fill"
                     objectFit="contain" objectPosition="left" />
             </div>
